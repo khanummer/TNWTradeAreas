d9147fad4257b66dbba0de51c7be0de83274659b
Ummer Khan <khanummergit@gmail.com>
2019-05-05 11:47:06 -0700

    adds styling to incentives

--- src/Incentives/Incentives.js
+++ src/Incentives/Incentives.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './Incentives.css';
+import './Incentives.css';
 import { withRouter } from 'react-router-dom';
 
 
@@ -8,26 +8,22 @@ class Incentives extends Component {
 
   render() {
     return (
-        <div>
-            Non-Traditional Incentives
+        <div className="Inc">
 
-            No franchise fee
-            No royalty for six months
-            Set of pizza ovens
-            Food credit or direct mail campaign (as applicable)
+            <div className="Inc-title">INDUSTRY LEADING INCENTIVES</div>
 
-            INDUSTRY LEADING Incentives
-
-            No Franchise Fee ($25,000 savings)
-            Set Of Ovens For Each Store
-            Reduced Royalties over 6 years as follows:
-            First 18 Months – 0.0% Royalty
-            Second 18 Months – 1.0% Royalty
-            Next 12 Months – 2.0% Royalty
-            Next 12 Months – 3.0% Royalty
-            Next 12 Months – 4.0% Royalty
-            After 6 years – Standard Rate of 5.0%
-            $3,000 Food Credit (when applicable)
+            <div className="Inc-div">No Franchise Fee ($25,000 savings)</div>
+            <div className="Inc-div">Set Of Ovens For Each Store</div>
+            <div className="Inc-div">Reduced Royalties over 6 years as follows:</div>
+            <ul className="list">
+              <li>First 18 Months – 0.0% Royalty</li>
+              <li>Second 18 Months – 1.0% Royalty</li>
+              <li> Next 12 Months – 2.0% Royalty</li>
+              <li> Next 12 Months – 3.0% Royalty</li>
+              <li> Next 12 Months – 4.0% Royalty</li>
+            </ul>
+            <div className="Inc-div">After 6 years – Standard Rate of 5.0%</div>
+            <div className="Inc-div">$3,000 Food Credit (when applicable)</div>
 
 
         </div>
